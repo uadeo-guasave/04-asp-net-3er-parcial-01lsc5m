9fda4813d69d51eb44fadd675e565645a2fc62e4
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2020-11-24 08:34:17 -0700

    3.04 Controladores
    Se definieron los controladores a utilizar.
    Pendiente de desarrollar las acciones.

---
+++ mywebapi/Controllers/AdminController.cs
@@ -0,0 +1,10 @@
+namespace mywebapi.Controllers
+{
+    [ApiController]
+    [Route("api/admin")]
+    class AdminController : ControllerBase
+    {
+        // TODO: endpoint para regresar la lista de pagos de un determinado periodo
+        // TODO: endpoint para iniciar sesión
+    }
+}
---
+++ mywebapi/Controllers/ReciboController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace mywebapi.Controllers
+{
+    [ApiController]
+    [Route("api/recibo")]
+    public class ReciboController : ControllerBase
+    {
+        // TODO: endpoint para consultar adeudo
+        // TODO: endpoint para registrar pago
+        // TODO: endpoint para imprimir comprobante
+    }
+}
