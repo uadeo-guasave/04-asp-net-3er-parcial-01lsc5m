04b835293cb2303f2fe31d3a7be5bdafb33ee8b4
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2020-12-10 07:58:23 -0700

    3.06 Actividad de cierre

--- mywebapi/Controllers/AdminController.cs
+++ mywebapi/Controllers/AdminController.cs
@@ -7,6 +7,13 @@ namespace mywebapi.Controllers
     class AdminController : ControllerBase
     {
         // TODO: endpoint para regresar la lista de pagos de un determinado periodo
-        // TODO: endpoint para iniciar sesión
+        //       recibe como parametros dos fechas (periodo)
+        //       utilizar el método POST para pasar los parametros
+        //       body request: {"inicio":"2020-12-01", "fin":"2020-12-31"}
+        //       https://localhost:5001/api/admin/periodo
+        //       la respuesta debe incluir el número de cuenta, nombre de usuario, importe
+        //       y los datos del pago
+        //       body response: [{"cuenta":123,"usuario":"Bidkar Aragón","importe":200.00,
+        //      "tarjeta":"123278163287","titular":"Bidkar","autorizacion":1232432,"fecha":2020-12-01}]
     }
 }
---
+++ mywebapi/Helpers/Periodo.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace mywebapi.Helpers
+{
+    public class Periodo
+    {
+        public DateTime Inicio { get; set; }
+        public DateTime Fin { get; set; }
+    }
+}
